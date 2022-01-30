3d14b10f58a0098c39dcf06a592a93e3932240b4
ruijadom <ruijadomingues@gmail.com>
2022-01-30 14:57:54 +0000

    feat: deprecation

--- src/schema.js
+++ src/schema.js
@@ -33,7 +33,7 @@ const typeDefs = gql`
     "The track's illustration to display in track card or track page detail"
     thumbnail: String
     "The track's approximate length to complete, in seconds"
-    length: Int @deprecated(reason: "Use durationInSeconds instead")  
+    length: Int @deprecated(reason: "Use durationInSeconds")  
     "The track´s full duration, in seconds"
     durationInSeconds: Int
     "The number of modules this track contains"
@@ -59,7 +59,7 @@ const typeDefs = gql`
     "The module's title"
     title: String!
     "The module's length in seconds"
-    length: Int @deprecated(reason: "Use durationInSeconds instead")
+    length: Int @deprecated(reason: "Use durationInSeconds")
     "The module´s video duration, in seconds"
     durationInSeconds: Int
     "The module's text-based description, can be in markdown format. In case of a video, it will be the enriched transcript"
